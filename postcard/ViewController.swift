0c4f1da0039902156cf72b48ed3caf1ad1081a6e
Develop <develop@Gregs-MacBook-Pro.local>
2014-11-11 11:54:39 -0700

    ios app postcard update challenge 1

--- postcard/ViewController.swift
+++ postcard/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -32,9 +33,17 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
         // enterMessageTextField.resignFirstResponder()
         enterMessageTextField.endEditing(true)
+        
+        enterNameTextField.text = ""
+        enterNameTextField.endEditing(true)
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
  
